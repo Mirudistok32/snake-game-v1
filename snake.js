b925c97372d78c5ee32cfe246007b46203dcc16e
Mirudistok <mirudistok32@yandex.ru>
2020-09-30 13:25:34 +0300

    Create move for snake and create food.js file

--- snake.js
+++ snake.js
@@ -1,24 +1,29 @@
-export const SNAKE_SPEED = 2;
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
 const snakeBody = [
   { x: 11, y: 11 },
   { x: 12, y: 11 },
   { x: 13, y: 11 },
   { x: 14, y: 11 },
+  { x: 15, y: 11 },
+  { x: 16, y: 11 },
 ];
 
 export function update() {
+  const inputDirectrion = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
-//   snakeBody[0].x += 0;
-//   snakeBody[0].y += 1;
+  snakeBody[0].x += inputDirectrion.x;
+  snakeBody[0].y += inputDirectrion.y;
 }
 export function draw(gameBoard) {
   snakeBody.forEach((segment) => {
     const snakeElement = document.createElement("div");
-    snakeElement.style.gridRowStart = segment.x;
-    snakeElement.style.gridColumnStart = segment.y;
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
   });
