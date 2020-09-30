771b71f6ef8383fd4c663e4639b4dc002d0157a6
Mirudistok <mirudistok32@yandex.ru>
2020-09-30 12:52:30 +0300

    create input.js

--- game.js
+++ game.js
@@ -1,23 +1,27 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake";
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake.js";
 
 let lasrRenderTime = 0;
-
-const main = (currentTime) => {
+const gameBoard = document.getElementById("game-board");
+function main(currentTime) {
+  window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lasrRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-  window.requestAnimationFrame(main);
   lasrRenderTime = currentTime;
 
+  console.log("main");
   update();
   draw();
-};
+}
 
 window.requestAnimationFrame(main);
 
 function update() {
+  console.log("update");
   updateSnake();
 }
 function draw() {
-  drawSnake();
+  console.log("draw");
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard);
 }
--- snake.js
+++ snake.js
@@ -1,7 +1,19 @@
 export const SNAKE_SPEED = 2;
-const snakeBody = [{ x: 11, y: 11 }];
+const snakeBody = [
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+  { x: 13, y: 11 },
+  { x: 14, y: 11 },
+];
 
-export function update() {}
+export function update() {
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+//   snakeBody[0].x += 0;
+//   snakeBody[0].y += 1;
+}
 export function draw(gameBoard) {
   snakeBody.forEach((segment) => {
     const snakeElement = document.createElement("div");
